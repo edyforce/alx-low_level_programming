0ad12c63c0d17967b8765e46a606d3dae8555242
ebenezer <sarbahebenezerdela@gmail.com>
2023-03-24 12:59:54 -0700

    1-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,10 +7,16 @@
 void positive_or_negative(int i)
 {
 	if (i < 0)
+	{
 		printf("%d is %s\n", i, "negative");
+	}
 	else if (i > 0)
+	{
 		printf("%d is %s\n", i, "positive");
-	else 
+	}
+	else
+	{ 
 		printf("%d is %s\n", i, "zero");
+	}
 	return;
 }
