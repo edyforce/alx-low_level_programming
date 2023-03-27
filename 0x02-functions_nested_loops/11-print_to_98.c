961082d772b7c7bd712c538b26e88351b4ee50f1
ebenezer <sarbahebenezerdela@gmail.com>
2023-03-26 22:11:55 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - print n to 98 counts
@@ -9,15 +10,16 @@
  * @n: input
 */
 
+void printf(counter, <= 98,);
 void print_to_98(int n)
 {
 	int counter;
 
 	if (n > 98)
 		for (counter = n; counter > 98; --counter)
-			printf ("%d, ", counter);
+			printf("%d, ", counter);
 	else
-		for (counter = n; counter < 98; ++counter)
+		for(counter = n; counter < 98; ++counter)
 			printf("%d, ", counter);
 	printf ("98\n");
 }
