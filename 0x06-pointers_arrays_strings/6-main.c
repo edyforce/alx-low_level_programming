3f77bce68f785e9170a6d220d98001e753bf09c1
ebenezer <sarbahebenezerdela@gmail.com>
2023-03-29 09:39:05 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Expect the best. Prepare for the worst.";
+	char str[] = "Capitalize on what comes.\nhello world! hello-world";
+	char str[] = "0123456hello world\thello world.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
